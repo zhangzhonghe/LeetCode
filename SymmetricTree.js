0a4b716eb414e36a5efeb00fd3e05ce14409aef9
Rairn <958414905@qq.com>
2023-02-08 11:51:17 +0800

    add iteratively for SymmetricTree

--- SymmetricTree.js
+++ SymmetricTree.js
@@ -22,3 +22,28 @@ function isMirror(left, right) {
         isMirror(left.left, right.right) &&
         isMirror(left.right, right.left);
 }
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetricIteratively = function(root) {
+    const q = [];
+    q.push(root);
+    q.push(root);
+
+    while(q.length) {
+        const node_1 = q.shift();
+        const node_2 = q.shift();
+
+        if (!node_1 && !node_2) continue;
+        if (!node_1 || !node_2) return false;
+        if (node_1.val !== node_2.val) return false;
+        q.push(node_1.left);
+        q.push(node_2.right);
+        q.push(node_1.right);
+        q.push(node_2.left);
+    }
+
+    return true;
+};

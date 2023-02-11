e36ab01e8fb0c778c05a62718f0c9a69f0961cc7
Rairn <958414905@qq.com>
2023-02-11 15:51:41 +0800

    medium: official answer

--- IntersectionOfTwoLinkedLists.js
+++ IntersectionOfTwoLinkedLists.js
@@ -23,3 +23,45 @@ function getIntersectionNode(headA, headB) {
   }
   return null;
 }
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+// function getIntersectionNode(headA, headB) {
+//   let nodeA = headA;
+//   let nodeB = headB;
+//   let n = 0;
+
+//   while (nodeA && nodeB) {
+//     if (nodeA === nodeB) return nodeA;
+//     nodeA = nodeA.next;
+//     nodeB = nodeB.next;
+//     if (n >= 2) continue;
+//     if (!nodeA) {
+//       nodeA = headB;
+//       n++;
+//     }
+//     if (!nodeB) {
+//       nodeB = headA;
+//       n++;
+//     }
+//   }
+
+//   return null;
+// }
+
+// 官方解法
+// var getIntersectionNode = function (headA, headB) {
+//   if (headA === null || headB === null) {
+//     return null;
+//   }
+//   let pA = headA,
+//     pB = headB;
+//   while (pA !== pB) {
+//     pA = pA === null ? headB : pA.next;
+//     pB = pB === null ? headA : pB.next;
+//   }
+//   return pA;
+// };

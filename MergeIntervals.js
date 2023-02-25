9040c057f3ef1f17ce84530542e735e974c53493
Rairn <958414905@qq.com>
2023-02-25 15:17:57 +0800

    refactor

--- MergeIntervals.js
+++ MergeIntervals.js
@@ -2,19 +2,23 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function (intervals) {
-  const result = [];
+function merge(intervals) {
+  const mergedIntervals = [];
+
   intervals.sort((a, b) => a[0] - b[0]);
+
   for (let i = 0; i < intervals.length; i++) {
     let k = 0;
+
     for (let j = i + 1; j < intervals.length; j++) {
       if (intervals[j][0] > intervals[i][1]) break;
       intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
       k++;
     }
-    result.push(intervals[i]);
+
+    mergedIntervals.push(intervals[i]);
     i += k;
   }
 
-  return result;
-};
+  return mergedIntervals;
+}
